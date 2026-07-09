076b093e7926a47313dc98481265dc5b409a93ca
yahircreativo <yahir.creativo@gmail.com>
2026-07-08 17:33:59 -0700

    v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     site: 'https://yahircreativo-art.github.io',
-    base: '/theKabobGuy',
+    base: '/thekabobguy',
     vite: {
         plugins: [tailwindcss()],
     },
