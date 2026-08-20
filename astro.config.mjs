cc8dc2920c2e09d650bff3069b1577e84648b25d
yahircreativo <yahir.creativo@gmail.com>
2026-08-20 12:32:51 -0700

    about-section-w-size

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 
 export default defineConfig({
-    site: 'https://thekabobguy.vercel.app',
+    site: 'https://thekabobguy.com',
     vite: {
         plugins: [tailwindcss()],
     },
